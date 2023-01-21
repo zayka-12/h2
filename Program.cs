2e769451f607307aa387ba30702ced4cde5c771c
zayka <120934899+zayka-12@users.noreply.github.com>
2023-01-21 17:59:17 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+Console.WriteLine("Введите 3 числа:");
+int number_1 = Convert.ToInt32(Console.ReadLine());
+int number_2 = Convert.ToInt32(Console.ReadLine());
+int number_3 = Convert.ToInt32(Console.ReadLine());
+
+int max = number_1;
+
+if (number_2 > max)
+{
+   max = number_2;
+}
+if (number_3 > max)
+{
+   max = number_3;
+}
